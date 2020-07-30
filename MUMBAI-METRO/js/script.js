1de21e7f5dc560fb876f9268d5982ede59903fa4
RAKESH <rakeshkumarsadotra@gmail.com>
2020-07-31 01:13:11 +0530

    webpage of Project on MUMBAI METRO

---
+++ MUMBAI-METRO/js/script.js
@@ -0,0 +1,14 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+$(document).on("click","#link",function(){
+ alert("E WALLET section is not working ! ");
+});
